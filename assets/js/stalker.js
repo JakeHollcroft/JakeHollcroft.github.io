5b28cc99c4543f0bb68c9d7bac448aeba841892a
Jake <jakehollcroft918@gmail.com>
2019-07-29 16:17:13 -0500

    first commit

---
+++ assets/js/stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+console.log("Hello there, " + firstName + " " + lastName + "! I see that you are " + age + " years old!")
